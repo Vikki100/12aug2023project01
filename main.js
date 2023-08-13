b327b299ee24a81ba3cdaa3de886dba5cf25ece9
Vikki100 <merasafar.13safar@gmail.com>
2023-08-13 14:44:24 +0530

    update page

--- main.js
+++ main.js
@@ -12,3 +12,11 @@ window.addEventListener("mousemove", function (detail) {
     circul.style.left = `${xvalue}px`
 
 })
+
+// 1 window me mousemove event lagaker mouse ki x and y position get kiye 
+// fir prticula div ko document.queryselector ki maddat se select kr ke div ki position ko mouse ke moveing 
+//  positon se set kiya x and y acces me
+
+                // circul.style.top = `${yvalue}px`
+                // circul.style.left = `${xvalue}px`
+
